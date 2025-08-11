9cf8d69ce8ad647d511dd2a932259e4fb3bdcbf5
edpiedra <edpiedra@mac.com>
2025-08-11 15:26:09 -0700

    fixed logger

--- install/scripts/20_opennisdk.sh
+++ install/scripts/20_opennisdk.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+SCRIPT_NAME=$(basename "$0")
+
 SCRIPTS_DIR="/home/pi/drone-rpi3/install/scripts"
 source "$SCRIPTS_DIR/00_common.env"
 source "$SCRIPTS_DIR/00_lib.sh"
