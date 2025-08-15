4517e95527731272cb90b471bea1b5584e9e8282
edpiedra <edpiedra@mac.com>
2025-08-14 17:22:22 -0700

    added required package for navio2 install

--- install/scripts/30_navio2.sh
+++ install/scripts/30_navio2.sh
@@ -10,7 +10,7 @@ source "$SCRIPTS_DIR/00_lib.sh"
 NAVIO2_GIT="https://github.com/emlid/Navio2.git"
 
 log "installing requirements..."
-sudo apt-get install -y -qq gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
+sudo apt-get install -y -qq gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf python3-venv
 
 if [ ! -f "$NAVIO2_WHEEL" ]; then 
     log "cloning from $NAVIO2_GIT..."
